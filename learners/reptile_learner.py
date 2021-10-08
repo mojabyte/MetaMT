0fcf0edeea986c004b096147c1b8a00f6bd5985f
M.Hassan Mojab <mh_mojab75@yahoo.com>
2021-10-08 16:01:24 +0330

    remove time prints

--- learners/reptile_learner.py
+++ learners/reptile_learner.py
@@ -1,4 +1,3 @@
-import time
 import torch
 
 
@@ -17,10 +16,7 @@ def reptile_learner(model, queue, optimizer, device, args):
     queue_length = len(queue)
     losses = 0
 
-    global_time = time.time()
-
     for i in range(queue_length):
-        task_time = time.time()
         for _ in range(args.update_step):
             optimizer.zero_grad()
 
@@ -47,32 +43,17 @@ def reptile_learner(model, queue, optimizer, device, args):
             #     optimizer.step()
             optimizer.step()
 
-        print(f"k-step update: {time.time() - task_time}")
-
-        parameters_time = time.time()
-
         for idx, param in enumerate(model.parameters()):
             running_vars[idx].data += param.data.clone()
             param.data = old_vars[idx].data.clone()
 
-        print(f"parameters update: {time.time() - parameters_time}")
-
-    print(f"queue train: {time.time() - global_time}")
-
-    calculate_params_time = time.time()
-
-    print(running_vars[0])
     for param in running_vars:
         param /= queue_length
-    print(running_vars[0])
 
     for idx, param in enumerate(model.parameters()):
         param.data = old_vars[idx].data + args.beta * (
             running_vars[idx].data - old_vars[idx].data
         )
-
-    print(f"calculate params: {time.time() - calculate_params_time}")
-    print("-" * 80)
 
     return losses / (queue_length * args.update_step)
 

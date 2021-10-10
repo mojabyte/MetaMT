600245b9232c6d11ef539d0a5b1cb57d2d451364
M.Hassan Mojab <mh_mojab75@yahoo.com>
2021-10-10 22:22:53 +0330

    add _copy method to update model parameters

--- learners/reptile_learner.py
+++ learners/reptile_learner.py
@@ -48,18 +48,17 @@ def reptile_learner(model, queue, optimizer, device, args):
             param.data = old_vars[idx].data.clone()
 
     update_param_time = time.time()
-    for param in running_vars:
-        param.data /= queue_length
+    with torch.no_grad():
+        for param in running_vars:
+            param.data /= queue_length
 
-    # for idx, param in enumerate(model.parameters()):
-    #     param.data = old_vars[idx].data + args.beta * (
-    #         running_vars[idx].data - old_vars[idx].data
-    #     )
+        # for idx, param in enumerate(model.parameters()):
+        #     param.data = old_vars[idx].data + args.beta * (
+        #         running_vars[idx].data - old_vars[idx].data
+        #     )
 
-    for idx, param in enumerate(model.parameters()):
-        param.data = (1 - args.beta) * old_vars[idx].data
-    for idx, param in enumerate(model.parameters()):
-        param.data += args.beta * running_vars[idx].data
+        for idx, param in enumerate(model.parameters()):
+            param.copy_((1 - args.beta) * old_vars[idx] + args.beta * running_vars[idx])
     print("update_param_time:", time.time() - update_param_time)
     return losses / (queue_length * args.update_step)
 

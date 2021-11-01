86ded4b09e8afe30b9bb9d3e0b6400e8c6cf70b0
M.Hassan Mojab <mh_mojab75@yahoo.com>
2021-11-01 14:08:06 +0330

    final fix

--- data.py
+++ data.py
@@ -64,18 +64,6 @@ class CorpusQA(Dataset):
                 threads=1,
             )
 
-            print(dataset[0])
-
-            dataset = {
-                "input_ids": dataset[:][0],
-                "attention_mask": dataset[:][1],
-                "token_type_ids": dataset[:][2],
-                "answer_start": dataset[:][3],
-                "answer_end": dataset[:][4],
-            }
-
-            print(dataset)
-
             torch.save(
                 {"features": features, "dataset": dataset, "examples": examples},
                 cached_features_file,
